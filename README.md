c2ff65ed2402b34c3b82f26d04e67b2d0d3ae4d5
KeithCCC <Keithc2chen@gmail.com>
2021-11-03 12:50:12 +0900

    IST update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # timezonemmc
 MMC timezone
+
+Timezone
+
+tzdict = {'Tokyo':'Asia/Tokyo', 
+            'St.Pete':'US/Eastern',
+            'Tampa':'US/Eastern',
+            'Paris':'Europe/Paris', 
+            'Vancouver':'America/Vancouver',
+            'Homburg':'Europe/Berlin',
+            'Milano':'Europe/Rome',
+            'London':'Europe/London',
+            'Dallas':'US/Central',
+            'San Jose':'US/Pacific',
+            'New York':'US/Eastern',
+            'Singapore':'Asia/Singapore',
+            'IST':'Asia/Kolkata'} 
+
+
+         choices=[
+            ('Tokyo', 'Tokyo'),
+            ('St.Pete', 'St.Pete'),
+            ('Paris', 'Paris'),
+            ('London', 'London'),
+            ('IST', 'IST'),
+            ('New York', 'New York'),
+            ('Singapore', 'Singapore'),
+            ('San Jose', 'San Jose')
+            ])           
--- main.py
+++ main.py
@@ -22,70 +22,75 @@ tzdict = {'Tokyo':'Asia/Tokyo',
             'San Jose':'US/Pacific',
             'New York':'US/Eastern',
             'Singapore':'Asia/Singapore',
-            'IST':'Asia/Kolkata'} 
+            'IST':'Asia/Kolkata',
+            'Sydney':'Australia/Sydney'} 
 class CityForm(Form):
     city1 = SelectField(u'Left', 
         choices=[
             ('Tokyo', 'Tokyo'),
-            ('St.Pete', 'St.Pete'),
-            ('Paris', 'Paris'),
-            ('London', 'London'),
-            ('IST', 'IST'),
-            ('New York', 'New York'),
             ('Singapore', 'Singapore'),
-            ('San Jose', 'San Jose')
+            ('IST', 'IST'),
+            ('Sydney','Sydney'),
+            ('London', 'London'),
+            ('New York', 'New York'),
+            ('Paris', 'Paris'),
+            ('San Jose', 'San Jose'),
+            ('St.Pete', 'St.Pete')
             ])
     city2 = SelectField(u'Center',
         choices=[
             ('Tokyo', 'Tokyo'),
-            ('St.Pete', 'St.Pete'),
-            ('Paris', 'Paris'),
-            ('London', 'London'),
-            ('IST', 'IST'),
-            ('New York', 'New York'),
             ('Singapore', 'Singapore'),
-            ('San Jose', 'San Jose')
-            ])            
+            ('IST', 'IST'),
+            ('Sydney','Sydney'),
+            ('London', 'London'),
+            ('New York', 'New York'),
+            ('Paris', 'Paris'),
+            ('San Jose', 'San Jose'),
+            ('St.Pete', 'St.Pete')
+            ])          
     city3 = SelectField(u'Right', 
         choices=[
             ('Tokyo', 'Tokyo'),
-            ('St.Pete', 'St.Pete'),
-            ('Paris', 'Paris'),
-            ('London', 'London'),
-            ('IST', 'IST'),
-            ('New York', 'New York'),
             ('Singapore', 'Singapore'),
-            ('San Jose', 'San Jose')
+            ('IST', 'IST'),
+            ('Sydney','Sydney'),
+            ('London', 'London'),
+            ('New York', 'New York'),
+            ('Paris', 'Paris'),
+            ('San Jose', 'San Jose'),
+            ('St.Pete', 'St.Pete')
             ])
     city4 = SelectField(u'Right', 
         choices=[
             ('Tokyo', 'Tokyo'),
-            ('St.Pete', 'St.Pete'),
-            ('Paris', 'Paris'),
-            ('London', 'London'),
-            ('IST', 'IST'),
-            ('New York', 'New York'),
             ('Singapore', 'Singapore'),
-            ('San Jose', 'San Jose')
+            ('IST', 'IST'),
+            ('Sydney','Sydney'),
+            ('London', 'London'),
+            ('New York', 'New York'),
+            ('Paris', 'Paris'),
+            ('San Jose', 'San Jose'),
+            ('St.Pete', 'St.Pete')
             ])
     submit = SubmitField('Submit')
 
 def BuildTimezoneList(time1st, label):
     #time.strftime("%m/%d %H:%M")
     timelist = [label]
-    timelist.append(time1st.strftime("%m/%d %a %H:00"))
+    timelist.append(time1st.strftime("%m/%d %a %H:%M"))
     for i in range(24):
         time1st = time1st + timedelta(hours=1)
-        timelist.append(time1st.strftime("%m/%d %a %H:00"))
+        timelist.append(time1st.strftime("%m/%d %a %H:%M"))
     return timelist
 
 def BuildTimezoneListnoYear(time1st, label):
     #time.strftime("%m/%d %H:%M")
     timelist = [label]
-    timelist.append(time1st.strftime("%a %H:00"))
+    timelist.append(time1st.strftime("%a %H:%M"))
     for i in range(24):
         time1st = time1st + timedelta(hours=1)
-        timelist.append(time1st.strftime("%a %H:00"))
+        timelist.append(time1st.strftime("%a %H:%M"))
     return timelist
 
 @app.route("/", methods=['GET', 'POST'])
@@ -99,7 +104,9 @@ def index():
         return resp
     else:
         citylist = cities.split(',')
-    
+        while len(citylist) < 4:
+            citylist.append('Tokyo')
+
     if form.validate_on_submit():
         city1 = form.city1.data
         city2 = form.city2.data
@@ -118,17 +125,25 @@ def index():
     cnt =4
     tzlist = list()
     loopCnt = 0
+    basetime =  datetime.now()
+    targettime =  datetime(basetime.year, basetime.month, basetime.day, basetime.hour, 00, 00, 0000)
     for i in citylist:
         if loopCnt == 0:
             try:
-                tzlist.append(BuildTimezoneList(datetime.now(pytz.timezone(tzdict[i])),i))
+                #tzlist.append(BuildTimezoneList(datetime.now(pytz.timezone(tzdict[i])),i))
+                #tzlist.append(BuildTimezoneList(targettime.now(pytz.timezone(tzdict[i])),i))
+                timezoneinfo=pytz.timezone(tzdict[i])
+                tzlist.append(BuildTimezoneList(targettime.astimezone(timezoneinfo),i))
             except:
                 resp = make_response(redirect('/'))
                 resp.set_cookie('cities', 'Tokyo,Singapore,IST,New York')
                 return resp
         else:
             try:
-                tzlist.append(BuildTimezoneListnoYear(datetime.now(pytz.timezone(tzdict[i])),i))
+                # tzlist.append(BuildTimezoneListnoYear(datetime.now(pytz.timezone(tzdict[i])),i))
+                # tzlist.append(BuildTimezoneListnoYear(targettime.now(pytz.timezone(tzdict[i])),i))
+                timezoneinfo=pytz.timezone(tzdict[i])
+                tzlist.append(BuildTimezoneListnoYear(targettime.astimezone(timezoneinfo),i))
             except:
                 resp = make_response(redirect('/'))
                 resp.set_cookie('cities', 'Tokyo,Singapore,IST,New York')
@@ -140,7 +155,6 @@ def index():
     form.city1.default = citylist[0]
     form.city2.default = citylist[1]
     form.city3.default = citylist[2]
-    print(citylist[3])
     form.city4.default = citylist[3]
     form.process()
     #return render_template('index.html',cat1=cat1list,cat2=cat2list,cat3=cat3list,cat4=cat4list, timenow=timenow, cnt=cnt, tzlist=timezonelist )
